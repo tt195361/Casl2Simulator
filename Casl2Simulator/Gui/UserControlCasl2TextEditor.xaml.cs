a4ef45af5b6a3f9109de33fb4482028c69c19859
tt195361 <tt195361@gmail.com>
2018-08-18 12:04:07 +0900

    UserControlCasl2TextEditor を追加。
    * MainWindow.xaml: 中身が UserControlCasl2TextEditor の LayoutDocument を追加。
    * UserControlCasl2TextEditor.xaml: 新規作成。avalonedit:TextEditor を格納する。
    * UserControlCasl2TextEditor.xaml.cs: 新規作成。
    * ICSharpCode.AvalonEdit.dll: avalonedit:TextEditor を使用するため追加。

---
+++ Casl2Simulator/Gui/UserControlCasl2TextEditor.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Controls;
+
+namespace Tt195361.Casl2Simulator.Gui
+{
+    /// <summary>
+    /// UserControlCasl2TextEditor.xaml の相互作用ロジック
+    /// </summary>
+    public partial class UserControlCasl2TextEditor : UserControl
+    {
+        /// <summary>
+        /// <see cref="UserControlCasl2TextEditor"/> のインスタンスを初期化します。
+        /// </summary>
+        public UserControlCasl2TextEditor()
+        {
+            InitializeComponent();
+        }
+    }
+}
